a3849770c8a59154da71977319d5ce71784f1735
Allan Furukita <furukitaallan@gmail.com>
2024-01-28 18:53:11 -0300

    feat: add code challenge

---
+++ lib/alert_messenger.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+const kAlertHeight = 80.0;
+
+enum AlertPriority {
+  error(2),
+  warning(1),
+  info(0);
+
+  const AlertPriority(this.value);
+  final int value;
+}
+
+class Alert extends StatelessWidget {
+  const Alert({
+    super.key,
+    required this.backgroundColor,
+    required this.child,
+    required this.leading,
+    required this.priority,
+  });
+
+  final Color backgroundColor;
+  final Widget child;
+  final Widget leading;
+  final AlertPriority priority;
+
+  @override
+  Widget build(BuildContext context) {
+    final statusbarHeight = MediaQuery.of(context).padding.top;
+    return Material(
+      child: Ink(
+        color: backgroundColor,
+        height: kAlertHeight + statusbarHeight,
+        child: Column(
+          children: [
+            SizedBox(height: statusbarHeight),
+            Expanded(
+              child: Row(
+                children: [
+                  const SizedBox(width: 28.0),
+                  IconTheme(
+                    data: const IconThemeData(
+                      color: Colors.white,
+                      size: 36,
+                    ),
+                    child: leading,
+                  ),
+                  const SizedBox(width: 16.0),
+                  Expanded(
+                    child: DefaultTextStyle(
+                      style: const TextStyle(color: Colors.white),
+                      child: child,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 28.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AlertMessenger extends StatefulWidget {
+  const AlertMessenger({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<AlertMessenger> createState() => AlertMessengerState();
+
+  static AlertMessengerState of(BuildContext context) {
+    try {
+      final scope = _AlertMessengerScope.of(context);
+      return scope.state;
+    } catch (error) {
+      throw FlutterError.fromParts(
+        [
+          ErrorSummary('No AlertMessenger was found in the Element tree'),
+          ErrorDescription(
+              'AlertMessenger is required in order to show and hide alerts.'),
+          ...context.describeMissingAncestor(
+              expectedAncestorType: AlertMessenger),
+        ],
+      );
+    }
+  }
+}
+
+class AlertMessengerState extends State<AlertMessenger>
+    with TickerProviderStateMixin {
+  late final AnimationController controller;
+  late final Animation<double> animation;
+
+  List<Alert> alerts = [];
+
+  final alertWidget = ValueNotifier<Alert?>(null);
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final alertHeight = MediaQuery.of(context).padding.top + kAlertHeight;
+    animation = Tween<double>(begin: -alertHeight, end: 0.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> showAlert({required Alert alert}) async {
+    if (alertWidget.value != null &&
+        alert.priority.value > alertWidget.value!.priority.value) {
+      alerts.removeAt(0);
+      await controller.reverse();
+    }
+    alerts.add(alert);
+    alerts.sort((a, b) => b.priority.value.compareTo(a.priority.value));
+    alertWidget.value = alerts.isNotEmpty ? alerts.first : null;
+    controller.forward();
+  }
+
+  Future<void> hideAlert() async {
+    await controller.reverse();
+    if (alerts.isNotEmpty) {
+      alerts.removeAt(0);
+      alertWidget.value = alerts.isNotEmpty ? alerts[0] : null;
+      if (alertWidget.value != null) controller.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final statusbarHeight = MediaQuery.of(context).padding.top;
+
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        final position = animation.value + kAlertHeight;
+        return Stack(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          children: [
+            Positioned.fill(
+              top: position <= statusbarHeight ? 0 : position - statusbarHeight,
+              child: _AlertMessengerScope(
+                state: this,
+                child: widget.child,
+              ),
+            ),
+            ValueListenableBuilder<Alert?>(
+                valueListenable: alertWidget,
+                builder: (context, value, _) {
+                  if (value != null) {
+                    return Positioned(
+                      top: animation.value,
+                      left: 0,
+                      right: 0,
+                      child: value,
+                    );
+                  }
+                  return const SizedBox();
+                })
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _AlertMessengerScope extends InheritedWidget {
+  const _AlertMessengerScope({
+    required this.state,
+    required super.child,
+  });
+
+  final AlertMessengerState state;
+
+  @override
+  bool updateShouldNotify(_AlertMessengerScope oldWidget) =>
+      state != oldWidget.state;
+
+  static _AlertMessengerScope? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<_AlertMessengerScope>();
+  }
+
+  static _AlertMessengerScope of(BuildContext context) {
+    final scope = maybeOf(context);
+    assert(scope != null, 'No _AlertMessengerScope found in context');
+    return scope!;
+  }
+}
